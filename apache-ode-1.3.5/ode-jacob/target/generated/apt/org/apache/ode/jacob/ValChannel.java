0770280ba342a57a948136c0336ead1a856495dd
michael.pantazoglou@gmail.com <michael.pantazoglou@gmail.com@f004a122-f478-6e0f-c15d-9ccb889b5864>
2012-04-27 11:29:56 +0000

    Imported in svn

---
+++ apache-ode-1.3.5/ode-jacob/target/generated/apt/org/apache/ode/jacob/ValChannel.java
@@ -0,0 +1,21 @@
+/*
+ * SOURCE FILE GENERATATED BY JACOB CHANNEL CLASS GENERATOR
+ * 
+ *               !!! DO NOT EDIT !!!! 
+ * 
+ * Generated On  : Fri Apr 27 13:30:03 EEST 2012
+ * For Interface : org.apache.ode.jacob.Val
+ */
+
+package org.apache.ode.jacob;
+
+/**
+ * An auto-generated channel interface for the channel type
+ * {@link org.apache.ode.jacob.Val}.
+ * @see org.apache.ode.jacob.Val
+ * @see org.apache.ode.jacob.ValChannelListener
+ */
+public interface ValChannel
+    extends org.apache.ode.jacob.Channel, 
+            org.apache.ode.jacob.Val
+{}
---
+++ apache-ode-1.3.5/ode-jacob/target/generated/apt/org/apache/ode/jacob/examples/eratosthenes/NaturalNumberStreamChannel.java
@@ -0,0 +1,21 @@
+/*
+ * SOURCE FILE GENERATATED BY JACOB CHANNEL CLASS GENERATOR
+ * 
+ *               !!! DO NOT EDIT !!!! 
+ * 
+ * Generated On  : Fri Apr 27 13:30:03 EEST 2012
+ * For Interface : org.apache.ode.jacob.examples.eratosthenes.NaturalNumberStream
+ */
+
+package org.apache.ode.jacob.examples.eratosthenes;
+
+/**
+ * An auto-generated channel interface for the channel type
+ * {@link org.apache.ode.jacob.examples.eratosthenes.NaturalNumberStream}.
+ * @see org.apache.ode.jacob.examples.eratosthenes.NaturalNumberStream
+ * @see org.apache.ode.jacob.examples.eratosthenes.NaturalNumberStreamChannelListener
+ */
+public interface NaturalNumberStreamChannel
+    extends org.apache.ode.jacob.Channel, 
+            org.apache.ode.jacob.examples.eratosthenes.NaturalNumberStream
+{}
